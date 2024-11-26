663682fa656a893196d1bdedd1b67b7bbee4db6a
nethanmcqinn <119746112+nethanmcqinn@users.noreply.github.com>
2024-11-26 20:03:04 +0800

    Delete PharmaCure/PharmaCure2 directory

--- PharmaCure/PharmaCure2/public/user_logout.php
+++
@@ -1,9 +0,0 @@
-<!-- user_logout.php -->
-<?php include '../includes/navbar.php'; ?>
-
-<?php
-session_start();
-session_destroy(); // Destroy all session data
-header("Location: user_login.php"); // Redirect to login page
-exit();
-?>
